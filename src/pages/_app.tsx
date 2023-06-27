862c7d1d19ba2857bcc7a34ca65077c75e56c950
Marcos Iglesias <golodhros@gmail.com>
2023-06-27 16:43:43 -0700

    feat: discord example:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,9 @@
 import '../styles/global.css';
 
 import type { AppProps } from 'next/app';
-import dynamic from 'next/dynamic';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <Component {...pageProps} />
 );
 
-export default dynamic(() => Promise.resolve(MyApp), {
-  ssr: false,
-});
+export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,152 @@
-import { Meta } from '@/layouts/Meta';
-import { Main } from '@/templates/Main';
+import { Meta } from "@/layouts/Meta";
+import { Main } from "@/templates/Main";
+
+import { useState, useEffect } from "react";
+
+const CellInput = ({ value, onValueChange }) => {
+  const handleOnChange = (e) => {
+    console.log("value", e.target.value);
+    onValueChange(e.target.value);
+  };
+
+  return (
+    <input
+      className="border border-2 border-gray-60"
+      type="text"
+      onChange={handleOnChange}
+      value={value}
+    />
+  );
+};
+
+const CellDisplay = ({ value }: any) => {
+  return <span className="border border-2 border-green-500">{value}</span>;
+};
+
+const Cell = ({ label, cellStateValue, onChange }) => {
+  const [inputValue, setInputValue] = useState<string>("");
+  const displayValue = cellStateValue.filter(
+    ({ label: cellLabel }) => cellLabel === label
+  )[0].value;
+
+  const handleInputValueChange = (value: string) => {
+    const parsedValue = parseInt(value, 10);
+
+    if (isNaN(parsedValue)) {
+      // Treat as a reference
+      const result = value
+        .split("")
+        .map((cellKey) => {
+          const cell = cellStateValue.filter(
+            ({ label: cellLabel }) => cellLabel === cellKey
+          )[0];
+          console.log("cell", cell);
+          const cellValue = cell.value;
+
+          return parseInt(cellValue, 10);
+        })
+        .reduce((acc, cellValue) => {
+          return acc + cellValue;
+        }, 0);
+      console.log("result", result);
+      onChange(result);
+    } else {
+      console.log("value", value);
+      setInputValue(value);
+    }
+  };
+
+  useEffect(() => {
+    // To finish
+    onChange(inputValue);
+  }, [inputValue]);
+
+  return (
+    <div>
+      <label>
+        {label}
+        <CellInput value={inputValue} onValueChange={handleInputValueChange} />
+      </label>
+      <CellDisplay value={displayValue} />
+    </div>
+  );
+};
+
+type CellState = {
+  label: "A" | "B" | "C" | "D";
+  value: number;
+};
+const initialCellDisplayValues: CellState[] = [
+  {
+    label: "A",
+    value: 0,
+  },
+  {
+    label: "B",
+    value: 0,
+  },
+  {
+    label: "C",
+    value: 0,
+  },
+  {
+    label: "D",
+    value: 0,
+  },
+];
+
+const Spreadsheet = () => {
+  const [cellStateValue, setCellStateValue] = useState<CellState[]>(
+    initialCellDisplayValues
+  );
+
+  const handleCellStateChange = (label: string, displayValue: number) => {
+    console.log("label", label);
+    console.log("displayValue", displayValue);
+    const updatedState = [
+      ...cellStateValue.filter(({ label: cellLabel }) => cellLabel !== label),
+      {
+        label,
+        value: displayValue,
+      },
+    ];
+
+    setCellStateValue(updatedState);
+  };
+
+  return (
+    <div className="spreadsheet">
+      <Cell
+        label="A"
+        cellStateValue={cellStateValue}
+        onChange={(displayValue) => {
+          handleCellStateChange("A", displayValue);
+        }}
+      />
+      <Cell
+        label="B"
+        cellStateValue={cellStateValue}
+        onChange={(displayValue) => {
+          handleCellStateChange("B", displayValue);
+        }}
+      />
+      <Cell
+        label="C"
+        cellStateValue={cellStateValue}
+        onChange={(displayValue) => {
+          handleCellStateChange("C", displayValue);
+        }}
+      />
+      <Cell
+        label="D"
+        cellStateValue={cellStateValue}
+        onChange={(displayValue) => {
+          handleCellStateChange("D", displayValue);
+        }}
+      />
+    </div>
+  );
+};
 
 const Index = () => {
   return (
@@ -11,10 +158,8 @@ const Index = () => {
         />
       }
     >
-      <h2 className="text-2xl font-bold">H2 Title</h2>
-      <p>Paragraph text</p>
-      <h3 className="text-lg font-semibold">H3 heading</h3>
-      <p>Paragraph text</p>
+      <h2 className="text-2xl font-bold">Spreadsheet</h2>
+      <Spreadsheet />
     </Main>
   );
 };
